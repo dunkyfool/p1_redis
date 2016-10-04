e9b4fe88893f700e294f9c9d1b79862ae2d7aece
dunkyfool <dunkyfool0113@gmail.com>
2016-10-04 16:50:42 +0800

    complete insert/transform/test function

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-def foo():
-    print '123'
+import redis
+import sys
 
-def rumor():
-    print '123'
+def insert(r,key,value):
+    pass
+    r.set(key,value)
+
+def transform(r):
+    pass
+    value, _type = None, None
+    key_list = r.keys()
+
+    for key in key_list:
+        _type = r.type(key)
+        if _type=='string':
+            value = r.get(key)
+            r.sadd('UNION',(key,value))
+
+def test(r):
+    pass
+    value, _type = None, None
+    _sep = ';'
+    key_list = r.keys()
+
+    for key in key_list:
+        _type = r.type(key)
+        if _type=='string': value = r.get(key)
+        elif _type=='set': value = r.smembers(key)
+        print 'KEY:',key,_sep,'TYPE',_type,_sep,'VALUE',value
+
 
 if __name__ == '__main__' :
-    foo()
+    pass
+    arg_list = sys.argv
+    r = redis.StrictRedis(host='localhost', port=30001)
+
+    if len(arg_list)==1:
+            print 'python main.py MODE [Key] [Value]'
+            print 'MODE:1 Insert Key & Value'
+            print 'MODE:2 Transform all String into Set'
+            print 'MODE:3 Output all Keys\' value'
+    else:
+        if arg_list[1]=='1': insert(r,arg_list[2],arg_list[3])
+        elif arg_list[1]=='2': transform(r)
+        elif arg_list[1]=='3': test(r)
