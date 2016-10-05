16d6e21fa6e88e305c1278eefba4d6da0e1f91c2
dunkyfool <dunkyfool0113@gmail.com>
2016-10-05 14:30:22 +0800

    Add Volume with redis.conf

--- launch.sh
+++ launch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+#version='backup'
+#docker build -t redis:$version .
+
 docker run \
+-v /home/dunkyfool/Projects/p1_redis/conf:/etc/redis \
 -p 30001:6379 \
 --net my-redis-cluster \
 --name test_redis \
--d redis redis-server \
+-d redis redis-server /etc/redis/redis.conf
 #--appendonly yes
