22364c60cf9e139765bfa3e7e2657b78113fd5c1
dunkyfool <dunkyfool0113@gmail.com>
2016-10-05 14:31:34 +0800

    stop and remove test_redis container

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import redis
 import sys
 
 def insert(r,key,value):
-    pass
+    '''
+    insert (key,value) as string into redis
+    input:
+        r: redis connection
+        key, value: input parameter
+    '''
     r.set(key,value)
 
 def transform(r):
-    pass
+    '''
+    transform all string(s) into set(UNION, (key,value))
+    input:
+        r:redis connction
+    '''
     value, _type = None, None
     key_list = r.keys()
 
@@ -16,8 +25,12 @@ def transform(r):
             value = r.get(key)
             r.sadd('UNION',(key,value))
 
-def test(r):
-    pass
+def print_str_set(r):
+    '''
+    output all key(s), its type & value
+    input:
+        r:redis connction
+    '''
     value, _type = None, None
     _sep = ';'
     key_list = r.keys()
@@ -28,6 +41,18 @@ def test(r):
         elif _type=='set': value = r.smembers(key)
         print 'KEY:',key,_sep,'TYPE',_type,_sep,'VALUE',value
 
+def mod_config(r):
+    '''
+    change config on the fly & rewrite redis.conf
+    input:
+        r:redis connction
+    '''
+    print r.config_get('maxmemory-policy')
+    print r.config_get('save')
+    print r.config_get('appendonly')
+
+    #r.config_set('appendonly','no')
+    #print r.config_get('appendonly')
 
 if __name__ == '__main__' :
     pass
@@ -39,7 +64,9 @@ if __name__ == '__main__' :
             print 'MODE:1 Insert Key & Value'
             print 'MODE:2 Transform all String into Set'
             print 'MODE:3 Output all Keys\' value'
+            print 'MODE:4 change config'
     else:
         if arg_list[1]=='1': insert(r,arg_list[2],arg_list[3])
         elif arg_list[1]=='2': transform(r)
-        elif arg_list[1]=='3': test(r)
+        elif arg_list[1]=='3': print_str_set(r)
+        elif arg_list[1]=='4': mod_config(r)
---
+++ rm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker stop test_redis && docker rm test_redis
