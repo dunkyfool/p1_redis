fadd6b27766e039eb797d5fd9789c95071dfaf78
dunkyfool <dunkyfool0113@gmail.com>
2016-10-06 13:43:30 +0800

    only attach volume/network/port

--- launch.sh
+++ launch.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-#version='backup'
-#docker build -t redis:$version .
-
 docker run \
--v /home/dunkyfool/redis/conf:/etc/redis/conf \
--v /home/dunkyfool/redis/data:/etc/redis/data \
--v /home/dunkyfool/redis/log:/etc/redis/log \
+-v /home/dunkyfool/redis:/mnt \
 -p 30001:6379 \
 --net my-redis-cluster \
 --name test_redis \
--d redis redis-server /etc/redis/conf/redis.conf \
---logfile /etc/redis/log/redis.log
+-d redis:backup redis-server /etc/redis/conf/redis.conf #\
+#--logfile /etc/redis/log/redis.log
