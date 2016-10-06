2d3ead84f194d0ff087417f0d708be89e98ecd64
dunkyfool <dunkyfool0113@gmail.com>
2016-10-06 13:47:25 +0800

    update

--- launch.sh
+++ launch.sh
@@ -5,5 +5,5 @@ docker run \
 -p 30001:6379 \
 --net my-redis-cluster \
 --name test_redis \
--d redis:backup redis-server /etc/redis/conf/redis.conf #\
+-d redis:backup #redis-server /etc/redis/conf/redis.conf #\
 #--logfile /etc/redis/log/redis.log
