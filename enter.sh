3cd9aa3eb241601c0702485f13bd5e841695cb31
dunkyfool <dunkyfool0113@gmail.com>
2016-10-06 13:41:58 +0800

    enter to sh shell

--- enter.sh
+++ enter.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #docker run -it --link test_redis:redis --rm redis redis-cli -h redis -p 6379
-docker exec -it test_redis redis-cli
+#docker exec -it test_redis redis-cli
+docker exec -it test_redis sh
